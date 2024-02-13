6eec68e14f564130ffb3608e0f9d6849a7731edf
unknown <szigetvarimarkjanos@gmail.com>
2024-02-13 17:39:48 +0100

    add css

--- olimpia/resources/views/index.blade.php
+++ olimpia/resources/views/index.blade.php
@@ -41,24 +41,27 @@
             @endforeach
         </tbody>
     </table>
+    <div class="container-keret">
+        <div class="content">
      <h3>Rövidpályás gyorskorcsolya a téli olimpiai játékokon</h3>
 
-          A 2022. évi téli olimpiai játékokon a rövidpályás gyorskorcsolya versenyszámait a pekingi Fővárosi Fedett Stadionban rendezték február 5. és 16. között. A férfiaknak és a nőknek egyaránt 4–4 versenyszámban, illetve egy vegyes versenyszámban osztottak érmeket. A vegyes 2000 méteres váltó először szerepelt a téli olimpiai játékok programjában. https://hu.wikipedia.org/wiki/R%C3%B6vidp%C3%A1ly%C3%A1s_gyorskorcsolya, Rövidpályás gyorskorcsolya 
+          A 2022. évi téli olimpiai játékokon a rövidpályás gyorskorcsolya versenyszámait a pekingi Fővárosi Fedett Stadionban rendezték február 5. és 16. között. A férfiaknak és a nőknek egyaránt 4–4 versenyszámban, illetve egy vegyes versenyszámban osztottak érmeket. A vegyes 2000 méteres váltó először szerepelt a téli olimpiai játékok programjában. <a href="https://hu.wikipedia.org/wiki/R%C3%B6vidp%C3%A1ly%C3%A1s_gyorskorcsolya">, Rövidpályás gyorskorcsolya </a> 
 
 
        <h3>Téli olimpia története</h3>
       <p>Az 1924. évi téli olimpiai játékok, később adott hivatalos nevén az I. téli olimpiai játékok egy több sportot magába foglaló nemzetközi sportesemény volt, melyet 1924. január 25. és február 5. között rendeztek meg a franciaországi Chamonix-ban. Hivatalosan Semaine Internationale des Sports d'Hiver (Télisportok Nemzetközi Hete) néven szerepelt az 1924. évi nyári olimpiai játékokkal, a Mont Blanc lábánál. Chamonix olimpiáját a Francia Olimpiai Bizottság rendezte meg, a várost a Nemzetközi Olimpiai Bizottság jelölte ki, mint az első téli olimpiai játékok házigazdáját. 1924-től egészen 1992-ig a téli olimpiákat a nyáriakkal megegyező években tartották.</p>
       <br>
           A kép címe: "Készítette: Auguste Matisse - precise immediate image source is unknown. The poster is also on display at the website of the IOC.,Közkincs"
-          <img src="img/1924WOlympicPoster.jpeg" alt=""><a href="https://commons.wikimedia.org/w/index.php?curid=216754">, Eredeti oldal <a/>
+          <img src="img/1924WOlympicPoster.jpeg" class="kep1"><a href="https://commons.wikimedia.org/w/index.php?curid=216754">, Eredeti oldal <a/>
                  
-<div>
+
          <h3>Rövid pályás gyorskorcsolya Magyarország</h3>
          <p>Magyarország a kínai Pekingben megrendezett 2022. évi téli olimpiai játékok egyik részt vevő nemzete volt. Az országot az olimpián 5 sportágban 14 sportoló képviselte. Magyarország először szerzett egynél több érmet a téli olimpiai játékok történetében. Liu Shaoang Magyarország első egyéni téli olimpiai aranyérmét nyerte.
-            <img src="img/magyar_rp_gyors_korcsolya.jpeg">
+            <img src="img/magyar_rp_gyors_korcsolya.jpeg" class="kep2">
          
         A kép címe: "A magyar érmeket szállító short trackesek (balról): Liu Shaoang, Jászapáti Petra, Kónya Zsófia, Liu Shaolin Sándor és Krueger John-Henry (Fotó: Árvai Károly)"
       Maga a sportág 1992 óta szerepel az olimpiákon, és eddig 16 ország tudott érmet szerezni az azóta lebonyolított kilenc játékokon. Közülük a belgák most iratkoztak fel egy bronzéremmel. Az élen itt is Dél-Korea áll 26 arannyal és 53 éremmel, a kínaiaknak 12 aranyuk és 37 dobogós helyezésük, a kanadaiaknak 10 aranyuk és 37 medáliájuk van. Innentől kezdve viszont nagyon szoros a mezőny: az amerikaiaknak 4 aranyuk, az olaszoknak, a hollandoknak és Oroszországnak 3-3 olimpiai bajnoki címe van, és Liu Shaoang 500-as aranyával, 1000 méteres bronzával és a vegyes váltó harmadik helyével Magyarország az igen előkelő 8. helyre lépett előre a tizedikről, megelőzve Japánt és Ausztráliát is. A mieink négy éve iratkoztak fel a tabellára a férfiváltó aranyérmével.    
       </div>
+    </div>
 </body>
 </html>
