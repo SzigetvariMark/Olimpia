5a94b7e61d8b50051a64576eb98cf26b741e5768
unknown <szigetvarimarkjanos@gmail.com>
2024-02-13 15:22:43 +0100

    add1

--- olimpia/resources/views/index.blade.php
+++ olimpia/resources/views/index.blade.php
@@ -1,88 +1,38 @@
 <!DOCTYPE html>
-
-<html>
+<html lang="en">
 <head>
-  <title>Rövidpályás gyorskorcsolya a téli olimpiai játékokon</title>
-  
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  
-  <script src="js/bootstrap.min.js"></script>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="olimpia.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Téli olimpiai játékok</title>
 </head>
 <body>
-
-
-
-<div>
-    <div>
-<h1>Téli olimpiai játékok</h1>
-    </div>
-    <div>
-    <div>
-        <h3>Kinai téli olompiai játékok</h3>
-        <p><img src=""/>
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac sapien volutpat, sagittis ligula a, vestibulum nulla. Vivamus leo quam, commodo ut ex in, ornare imperdiet turpis. Vivamus aliquam leo vitae turpis tempus, quis imperdiet lectus volutpat. Nam quis turpis eu eros ullamcorper laoreet non non nunc. Nam id turpis quis risus sodales ullamcorper. Nullam ipsum mauris, gravida vel ornare ut, efficitur at lacus. Mauris velit ante, laoreet vel magna ut, auctor finibus libero. Duis vel diam metus. Nam a velit a nunc dictum pulvinar id vel nunc. Etiam tristique eu ligula aliquam dapibus. Cras consequat ligula sit amet tortor sodales tristique. Aliquam dignissim tortor eu ante laoreet vulputate. Quisque maximus nulla id nulla hendrerit ultricies. Duis eu nisl ullamcorper, egestas quam ac, facilisis augue. 
-        </p>
-        <div>
-            <div>
-                <h3>Helyszinek:</h3>
-                <table>
-                    <thead>
-                    <tr>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
-  </div>
-
-
-<div>
-  <div>
-      <div>
-          <h3>Rövidpályás gyorskorcsolya a téli olimpiai játékokon</h3>
-
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac sapien volutpat, sagittis ligula a, vestibulum nulla. Vivamus leo quam, commodo ut ex in, ornare imperdiet turpis. Vivamus aliquam leo vitae turpis tempus, quis imperdiet lectus volutpat. Nam quis turpis eu eros ullamcorper laoreet non non nunc. Nam id turpis quis risus sodales ullamcorper. Nullam ipsum mauris, gravida vel ornare ut, efficitur at lacus. Mauris velit ante, laoreet vel magna ut, auctor finibus libero. Duis vel diam metus. Nam a velit a nunc dictum pulvinar id vel nunc. Etiam tristique eu ligula aliquam dapibus. Cras consequat ligula sit amet tortor sodales tristique. Aliquam dignissim tortor eu ante laoreet vulputate. Quisque maximus nulla id nulla hendrerit ultricies. Duis eu nisl ullamcorper, egestas quam ac, facilisis augue. 
-      </div>
-    <div>
-      <h3>Téli olimpia története</h3>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac sapien volutpat, sagittis ligula a, vestibulum nulla. Vivamus leo quam, commodo ut ex in, ornare imperdiet turpis. Vivamus aliquam leo vitae turpis tempus, quis imperdiet lectus volutpat. Nam quis turpis eu eros ullamcorper laoreet non non nunc. Nam id turpis quis risus sodales ullamcorper. Nullam ipsum mauris, gravida vel ornare ut, efficitur at lacus. Mauris velit ante, laoreet vel magna ut, auctor finibus libero. Duis vel diam metus. Nam a velit a nunc dictum pulvinar id vel nunc. Etiam tristique eu ligula aliquam dapibus. Cras consequat ligula sit amet tortor sodales tristique. Aliquam dignissim tortor eu ante laoreet vulputate. Quisque maximus nulla id nulla hendrerit ultricies. Duis eu nisl ullamcorper, egestas quam ac, facilisis augue. 
-    </div>
-
-    <div>
-      <h3>Rövid pályás gyorskorcsolya Magyarország</h3>
+    <nav>
+    <h1>Téli olimpiai játékok</h1>
+    </nav>
+<h3>Kinai téli olompiai játékok</h3>
         
-    </div>      
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac sapien volutpat, sagittis ligula a, vestibulum nulla. Vivamus leo quam, commodo ut ex in, ornare imperdiet turpis. Vivamus aliquam leo vitae turpis tempus, quis imperdiet lectus volutpat. Nam quis turpis eu eros ullamcorper laoreet non non nunc. Nam id turpis quis risus sodales ullamcorper. Nullam ipsum mauris, gravida vel ornare ut, efficitur at lacus. Mauris velit ante, laoreet vel magna ut, auctor finibus libero. Duis vel diam metus. Nam a velit a nunc dictum pulvinar id vel nunc. Etiam tristique eu ligula aliquam dapibus. Cras consequat ligula sit amet tortor sodales tristique. Aliquam dignissim tortor eu ante laoreet vulputate. Quisque maximus nulla id nulla hendrerit ultricies. Duis eu nisl ullamcorper, egestas quam ac, facilisis augue. 
+    <img src="img/2022_Winter_Olympics_logo.svg">  <a href="https://upload.wikimedia.org/wikipedia/commons/b/b4/2022_Winter_Olympics_logo.svg">  eijing2022.cn, <a/> Public domain, via Wikimedia Commons 
+            A XXIV. téli olimpiai játékokat 2022. február 4. és február 20. között rendezték a kínai Pekingben. A rendező várost a 128. IOC gyűlésen választotta ki a Nemzetközi Olimpiai Bizottság 2015. július 31-én a malajziai Kuala Lumpurban. A 2022-es téli olimpiának 25 helyszíne van. A bázis Pekingben található, amely otthont ad például a jégkorong, a gyorskorcsolya, a curling versenyeknek. A 2022. február 4. és 20. között zajló pekingi téli olimpiai játékok egyetlen új, a játékokra emelt állandó helyszíne az Ice Ribbon, azaz Jégszalag.
+            Peking a Kínai Népköztársaság fővárosa, négy tartományi jogú városának egyike, a Pekingi főegyházmegye érseki székvárosa. Pekinget északról, nyugatról, délről és kis részben keletről Hopej tartomány határolja, míg délkeletről Tiencsin tartománnyal szomszédos.
 
-   </div>
-</div>
-    <div class="container-fluid">
-       <div>
-           <div>
-                 <h3>Éremszerző helyek listálya:</h3>
-                   <table class="table table-striped">
-                     <thead>
-                       <tr>
-                       
-                      </tr>
-                    </thead>
-                   <tbody>
-                    
-                </tbody>
-            </table>
-           </div>
-           <div class="col-md-5">
-           <h3>Magyar éremszerzők listája</h3>
-                <h6>A kiválasztott éremszerző adatai jelennek meg.</h6>
-                
-           </div>
-       </div>
-    </div>
+     h3>Rövidpályás gyorskorcsolya a téli olimpiai játékokon</h3>
+
+          A 2022. évi téli olimpiai játékokon a rövidpályás gyorskorcsolya versenyszámait a pekingi Fővárosi Fedett Stadionban rendezték február 5. és 16. között. A férfiaknak és a nőknek egyaránt 4–4 versenyszámban, illetve egy vegyes versenyszámban osztottak érmeket. A vegyes 2000 méteres váltó először szerepelt a téli olimpiai játékok programjában. https://hu.wikipedia.org/wiki/R%C3%B6vidp%C3%A1ly%C3%A1s_gyorskorcsolya, Rövidpályás gyorskorcsolya 
+
+
+       <h3>Téli olimpia története</h3>
+      <p>Az 1924. évi téli olimpiai játékok, később adott hivatalos nevén az I. téli olimpiai játékok egy több sportot magába foglaló nemzetközi sportesemény volt, melyet 1924. január 25. és február 5. között rendeztek meg a franciaországi Chamonix-ban. Hivatalosan Semaine Internationale des Sports d'Hiver (Télisportok Nemzetközi Hete) néven szerepelt az 1924. évi nyári olimpiai játékokkal, a Mont Blanc lábánál. Chamonix olimpiáját a Francia Olimpiai Bizottság rendezte meg, a várost a Nemzetközi Olimpiai Bizottság jelölte ki, mint az első téli olimpiai játékok házigazdáját. 1924-től egészen 1992-ig a téli olimpiákat a nyáriakkal megegyező években tartották.</p>
+      <br>
+          A kép címe: "Készítette: Auguste Matisse - precise immediate image source is unknown. The poster is also on display at the website of the IOC.,Közkincs"
+          <img src="img/1924WOlympicPoster.jpeg" alt=""><a href="https://commons.wikimedia.org/w/index.php?curid=216754">, Eredeti oldal <a/>
+                 
+
+         <h3>Rövid pályás gyorskorcsolya Magyarország</h3>
+         <p>Magyarország a kínai Pekingben megrendezett 2022. évi téli olimpiai játékok egyik részt vevő nemzete volt. Az országot az olimpián 5 sportágban 14 sportoló képviselte. Magyarország először szerzett egynél több érmet a téli olimpiai játékok történetében. Liu Shaoang Magyarország első egyéni téli olimpiai aranyérmét nyerte.
+            <img src="img/magyar_rp_gyors_korcsolya.jpeg">
+         
+        A kép címe: "A magyar érmeket szállító short trackesek (balról): Liu Shaoang, Jászapáti Petra, Kónya Zsófia, Liu Shaolin Sándor és Krueger John-Henry (Fotó: Árvai Károly)"
+      Maga a sportág 1992 óta szerepel az olimpiákon, és eddig 16 ország tudott érmet szerezni az azóta lebonyolított kilenc játékokon. Közülük a belgák most iratkoztak fel egy bronzéremmel. Az élen itt is Dél-Korea áll 26 arannyal és 53 éremmel, a kínaiaknak 12 aranyuk és 37 dobogós helyezésük, a kanadaiaknak 10 aranyuk és 37 medáliájuk van. Innentől kezdve viszont nagyon szoros a mezőny: az amerikaiaknak 4 aranyuk, az olaszoknak, a hollandoknak és Oroszországnak 3-3 olimpiai bajnoki címe van, és Liu Shaoang 500-as aranyával, 1000 méteres bronzával és a vegyes váltó harmadik helyével Magyarország az igen előkelő 8. helyre lépett előre a tizedikről, megelőzve Japánt és Ausztráliát is. A mieink négy éve iratkoztak fel a tabellára a férfiváltó aranyérmével.    
 </body>
 </html>
-
